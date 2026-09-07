edb887556161b28b775d7f464ebc050866d65f00
Jeremy E. Baun <jeremybaun1220@gmail.com>
2026-09-06 20:26:05 -0700

    Add first clinic supply record

---
+++ sql/03_insert_supplies.sql
@@ -0,0 +1,14 @@
+INSERT INTO supplies (
+    supply_name,
+    department,
+    quantity,
+    unit_cost,
+    last_updated
+)
+VALUES (
+    'Nitrile Gloves',
+    'ER',
+    200,
+    12.50,
+    '2026-09-06'
+);
