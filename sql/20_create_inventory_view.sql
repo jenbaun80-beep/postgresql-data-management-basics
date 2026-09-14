477657cdc3c9803979fc04db7c381491b59ecab7
Jeremy E. Baun <jeremybaun1220@gmail.com>
2026-09-13 22:01:07 -0700

    Complete Milestone 20: SQL Views

--- sql/20_create_inventory_view.sql
+++ sql/20_create_inventory_view.sql
@@ -7,4 +7,12 @@ SELECT d.department_name,
 FROM supplies s
 JOIN departments d
     ON s.department_id = d.department_id
-    
+;
+
+-- Create department stock summary view
+
+CREATE VIEW department_stock_summary AS
+SELECT department_name, SUM(quantity) AS total_stock
+FROM supply_inventory_view
+GROUP BY department_name;
+
